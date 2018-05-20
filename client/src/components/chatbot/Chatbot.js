9c92cb5117af8f6e47f66f0093481ab1089de084
Jana Bergant <jana.bergant@gmail.com>
2018-05-20 19:22:17 +0200

    S5 - 010 - Simple Message component

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
 import axios from "axios/index";
+
+
+import Message from './Message';
 
 
 class Chatbot extends Component {
     constructor(props) {
         super(props);
-
+        // This binding is necessary to make `this` work in the callback
+        this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
         this.state = {
             messages: []
         };
@@ -57,16 +60,39 @@ class Chatbot extends Component {
         }
     };
 
+    componentDidMount() {
+        this.df_event_query('Welcome');
+    }
 
+    _handleInputKeyPress(e) {
+        if (e.key === 'Enter') {
+            this.df_text_query(e.target.value);
+            e.target.value = '';
+        }
+    }
+
+
+
+
+    renderMessages(returnedMessages) {
+        if (returnedMessages) {
+            return returnedMessages.map((message, i) => {
+                    return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>;
+                }
+            )
+        } else {
+            return null;
+        }
+    }
 
     render() {
         return (
             <div style={{ height: 400, width:400, float: 'right'}}>
                 <div id="chatbot"  style={{ height: '100%', width:'100%', overflow: 'auto'}}>
                     <h2>Chatbot</h2>
-                    Here we will display messages
+                    {this.renderMessages(this.state.messages)}
                 </div>
-                <input type="text"  />
+                <input type="text" onKeyPress={this._handleInputKeyPress} />
             </div>
         );
     }
