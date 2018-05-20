53887bea4457c6fca516d3ac4bece3254f56569f
Jana Bergant <jana.bergant@gmail.com>
2018-05-20 19:19:35 +0200

    S5 - 009 – Add API calls to backend APP

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,10 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import axios from "axios/index";
 
 
-const Header = () => {
-    return (
-        <h2>Chatbot will be here</h2>
-    );
-};
+class Chatbot extends Component {
+    constructor(props) {
+        super(props);
 
-export default Header;
+        this.state = {
+            messages: []
+        };
+    }
+
+
+    async df_text_query (queryText) {
+        let msg;
+        let says = {
+            speaks: 'user',
+            msg: {
+                text : {
+                    text: queryText
+                }
+            }
+        }
+        this.setState({ messages: [...this.state.messages, says]});
+        const res = await axios.post('/api/df_text_query',  {text: queryText});
+
+        if (res.data.fulfillmentMessages ) {
+            for (let i = 0; i < res.data.fulfillmentMessages.length; i++) {
+                msg = res.data.fulfillmentMessages[i];
+                says = {
+                    speaks: 'bot',
+                    msg: msg
+                }
+                this.setState({ messages: [...this.state.messages, says]});
+            }
+        }
+    };
+
+
+    async df_event_query(eventName) {
+
+        const res = await axios.post('/api/df_event_query',  {event: eventName});
+        let msg, says = {};
+
+        if (res.data.fulfillmentMessages ) {
+            for (let i=0; i<res.data.fulfillmentMessages.length; i++) {
+                msg = res.data.fulfillmentMessages[i];
+                says = {
+                    speaks: 'bot',
+                    msg: msg
+                }
+
+                this.setState({ messages: [...this.state.messages, says]});
+            }
+        }
+    };
+
+
+
+    render() {
+        return (
+            <div style={{ height: 400, width:400, float: 'right'}}>
+                <div id="chatbot"  style={{ height: '100%', width:'100%', overflow: 'auto'}}>
+                    <h2>Chatbot</h2>
+                    Here we will display messages
+                </div>
+                <input type="text"  />
+            </div>
+        );
+    }
+
+
+
+}
+
+
+export default Chatbot;
