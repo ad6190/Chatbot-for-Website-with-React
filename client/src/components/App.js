defe92b5ac83f19566705e4718e4ac10f6a92a83
Jana Bergant <jana.bergant@gmail.com>
2018-06-12 10:34:54 +0200

    S9 - 005 - app to function component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 
@@ -7,25 +7,21 @@ import Landing from './Landing';
 import Shop from './shop/Shop';
 import Chatbot from './chatbot/Chatbot';
 
-class App extends Component {
-
-
-    render() {
-        return (
-            <div className="container">
-                <BrowserRouter>
-                    <div>
-                        <Header />
-                        <Route exact path="/" component={Landing} />
-                        <Route exact path="/shop" component={Shop} />
-                        <Chatbot />
-                    </div>
-                </BrowserRouter>
-
+const App = () => (
+    <div className="container">
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <Route exact path="/" component={Landing}/>
+                <Route exact path="/shop" component={Shop}/>
+                <Chatbot/>
             </div>
-        );
-    }
-}
+        </BrowserRouter>
+
+    </div>
+);
+
+
 
 export default App;
 
